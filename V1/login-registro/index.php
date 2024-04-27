3428c76f6c1190f574bb1fe1d25bc0a113068f24
ROKO <89106878+ROKOPM@users.noreply.github.com>
2024-04-27 16:33:42 -0600

    Login_bd

--- V1/login-registro/index.php
+++ V1/login-registro/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login y Register - MagtimusPro</title>
+    <title>Login y Register</title>
     
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
@@ -39,12 +39,12 @@
                     </form>
 
                     <!--Register-->
-                    <form action="" class="formulario__register">
+                    <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
                         <h2>Regístrarse</h2>
-                        <input type="text" placeholder="Nombre completo">
-                        <input type="text" placeholder="Correo Electronico">
-                        <input type="text" placeholder="Usuario">
-                        <input type="password" placeholder="Contraseña">
+                        <input type="text" placeholder="Nombre completo" name="nombre_completo">
+                        <input type="text" placeholder="Correo Electronico" name="correo">
+                        <input type="text" placeholder="Usuario" name="usuario">
+                        <input type="password" placeholder="Contraseña" name="contrasena">
                         <button>Regístrarse</button>
                     </form>
                 </div>
---
+++ V1/login-registro/php/registro_usuario_be.php
@@ -0,0 +1,32 @@
+<?php
+
+    include 'conexion_be.php';
+
+    $nombre_completo = $_POST['nombre_completo'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena) 
+               VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
+    
+    $ejecutar = mysqli_query($conexion, $query);
+
+    if($ejecutar){
+        echo '
+            <script>
+                alert("Usuario almacenado exitosamente");
+                window.location = "../index.php";
+            </script>
+        ';
+    }else{
+        echo '
+            <script>
+                alert("Intentalo de nuevo, usuario no almacenado");
+                window.location = "../index.php";
+            </script>
+        ';
+    }
+
+    mysqli_close($conexion);
+?>
