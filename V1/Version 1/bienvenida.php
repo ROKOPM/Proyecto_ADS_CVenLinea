8cc892eb3117494cefd7bbb5f94282429f6a6ff1
ROKO <89106878+ROKOPM@users.noreply.github.com>
2024-06-22 18:13:38 -0600

    Update bienvenida.php

--- V1/Version 1/bienvenida.php
+++ V1/Version 1/bienvenida.php
@@ -1,9 +1,8 @@
 <?php
-
     session_start();
 
-    if(!isset($_SESSION['usuario'])){
-        echo'
+    if (!isset($_SESSION['usuario'])) {
+        echo '
             <script>
                 alert("Por favor debes iniciar sesión");
                 window.location = "index.php";
@@ -15,14 +14,14 @@
 ?>  
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido al proyecto</title> 
-    <a href="php/cerrar_sesion.php">cerrar sesión</a>
+    <title>Bienvenido al proyecto</title>
 </head>
 <body>
     <h1>Bienvenido</h1>
+    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
 </body>
 </html>
