8ed1bae326bb0ac2d43ac25cefa8be1cd05ef04b
ROKO <89106878+ROKOPM@users.noreply.github.com>
2024-06-22 20:33:38 -0600

    Add files via upload

---
+++ Version 1/php/conexion_be.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db")
+
+    /*
+    id($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de datos';
+    }*/
+
+
+?>
