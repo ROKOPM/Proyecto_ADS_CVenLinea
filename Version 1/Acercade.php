36320765979a36b1b12092b48ab4aad5935b8e6f
Adriana Rocha García <164675791+AdrianaRochaGarcia@users.noreply.github.com>
2024-06-27 02:09:40 -0600

    Update Acercade.php

--- Version 1/Acercade.php
+++ Version 1/Acercade.php
@@ -120,7 +120,7 @@
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
                 <!-- Logo o Nombre del sitio -->
-                <a href="#" class="d-flex align-items-center gap-2 text-decoration-none">
+                <a href="index.php" class="d-flex align-items-center gap-2 text-decoration-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
                         <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                         <polyline points="9 22 9 12 15 12 15 22"></polyline>
@@ -142,7 +142,7 @@
                                     <a class="nav-link" href="Acercade.php">Acerca de nosotros</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="Contacto.html">Contacto</a>
+                                    <a class="nav-link" href="Contacto.php">Contacto</a>
                                 </li>
                             </ul>
                         </div>
