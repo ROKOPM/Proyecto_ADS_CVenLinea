8d70bd8237933429b108b0cff16d4b69fa64ee2b
Adriana Rocha García <164675791+AdrianaRochaGarcia@users.noreply.github.com>
2024-06-27 02:07:39 -0600

    Update Contacto.php

--- Version 1/Contacto.php
+++ Version 1/Contacto.php
@@ -207,7 +207,7 @@ ul.breadcrumb li a.active:hover {
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
                     <!-- Logo o Nombre del sitio -->
-                    <a href="#" class="d-flex align-items-center gap-2 text-decoration-none">
+                    <a href="index.php" class="d-flex align-items-center gap-2 text-decoration-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
                             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                             <polyline points="9 22 9 12 15 12 15 22"></polyline>
